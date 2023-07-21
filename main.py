4ba54c8edbd7f8cc7589f98e4c20645c8f485192
Vikram <vikramvikky2003@gmail.com>
2023-07-22 00:32:16 +0530

    Add language and url

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 from send_email import send_email
 
 api_key = "d9f8e660313c4c9a84503123c88cae47"
-url = "https://newsapi.org/v2/everything?q=tesla&from \
-        =2023-06-21&sortBy=publishedAt&apiKey=d9f8e660313c4c9a84503123c88cae47"
+url = "https://newsapi.org/v2/everything?" \
+      "q=tesla&" \
+      "sortBy=publishedAt&" \
+      "apiKey=d9f8e660313c4c9a84503123c88cae47&language=en"
 
 # Make request
 req = requests.get(url)
@@ -15,9 +17,11 @@ content = req.json()
 
 body = ""
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" + \
+               article["description"] + "\n" + \
+               article["url"] + 2*"\n"
 
 body = body.encode('utf-8')
 send_email(message=body)
